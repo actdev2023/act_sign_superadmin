691b26d5171312b2c9c415818e882c08db11b3f6
iamcryschei <71592236+iamcryschei@users.noreply.github.com>
2023-08-09 10:22:28 +0800

    update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -109,12 +109,12 @@ const App = (props: ExtendedAppProps) => {
     
       <CacheProvider value={emotionCache}>
         <Head>
-          <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+          <title>ACT MED SIGN ONLINE</title>
           <meta
             name='description'
-            content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+            content='ACT MED SIGN ONLINE'
           />
-          <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+          <meta name='keywords' content='ACT MED SIGN ONLINE' />
           <meta name='viewport' content='initial-scale=1, width=device-width' />
         </Head>
 
