3e4108e46a11e1b5e7e14775f8ed4fe49c34ad69
iamcryschei <71592236+iamcryschei@users.noreply.github.com>
2023-09-06 07:44:26 +0800

    update config api

--- src/configs/config.tsx
+++ src/configs/config.tsx
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: "http://localhost:8080",
+    API_URL: "https://api.isigndocu.com",
     // API_URL: "http://ec2-54-177-192-181.us-west-1.compute.amazonaws.com:8080",
     CLIENT_ID: "",
     CLIENT_SECRET: ""
